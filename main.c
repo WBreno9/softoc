009fab18e7647bad339af339f78aac02cd7f2a8f
Willaiam Breno <williambreno@mat.ci.ufpb.br>
2018-08-31 15:41:22 -0300

    WIP

--- main.c
+++ main.c
@@ -9,17 +9,16 @@
 
 //Fits in a 32bit register
 typedef struct {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-    unsigned char a;
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
 } pixel_t;
 
 typedef struct {
-    pixel_t* pixels;
+    uint8_t* data;
 
-    uint32_t w;
-    uint32_t h;
+    size_t w;
+    size_t h;
 } image_t;
 
 image_t* create_image(uint32_t w, uint32_t h) {
@@ -27,25 +26,29 @@ image_t* create_image(uint32_t w, uint32_t h) {
     t->w = w;
     t->h = h;
 
-    t->pixels = (pixel_t*)malloc(sizeof(pixel_t)*w*h);
+        t->data = (uint8_t*)malloc(w*h*3);
 
     return t;
 }
 
 void free_image(image_t* t) {
-    free(t->pixels);
+    free(t->data);
     free(t);
 }
 
 void write_pixel(image_t* t, pixel_t p, uint32_t x, uint32_t y) {
-    uint32_t index = x + y * t->w;
+    uint32_t index = 3*(x + y * t->w);
 
-    t->pixels[index] = p;
+    t->data[index    ] = p.r;
+    t->data[index + 1] = p.g;
+    t->data[index + 2] = p.b;
 }
 
 void clear_image(image_t* t, pixel_t p) {
-    for (int i = 0; i < t->w * t->h; i++) {
-        t->pixels[i] = p;
+    for (int x = 0; x < t->w; x++) {
+        for (int y = 0; y < t->h; y++) {
+            write_pixel(t, p, x, y);
+        }
     }
 }
 
@@ -57,9 +60,10 @@ int write_image(image_t* t, const char* file_path) {
         return 0;
     }
 
+    //PPM image
     //Byte (P6) header
-    fprintf(file, "P6\n %d %d\n 255\n", t->w, t->h);
-    fwrite(t->pixels, sizeof(pixel_t), t->w * t->h, file);
+    fprintf(file, "P6\n %lu %lu\n 255\n", t->w, t->h);
+    fwrite(t->data, 1, t->w * t->h * 3, file);
 
     return 1;
 }
@@ -90,7 +94,7 @@ typedef __m128 vec4;
 #define VEC_STORE_ALL           0x0F
 
 static inline void vec4_print(vec4 a) {
-    printf("vec4 = [%.2f, %.2f, %.2f, %.2f]\n", a[0], a[1], a[2], a[3]);
+    printf("vec4 = [%.4f, %.4f, %.4f, %.4f]\n", a[0], a[1], a[2], a[3]);
 }
 
 static inline vec4 _vec4_crs(vec4 a, vec4 b) {
@@ -133,13 +137,9 @@ static inline void mat4_print(mat4 m) {
 }
 
 /*TODO*/
-//Check out _MM_TRANSPOSE4_PS
-//It probably uses shuffles
-static inline void mat4_transpose(mat4 a, mat4 b) {
-    b[0] = vec4_set(a[0][0], a[1][0], a[2][0], a[3][0]);
-    b[1] = vec4_set(a[0][1], a[1][1], a[2][1], a[3][1]);
-    b[2] = vec4_set(a[0][2], a[1][2], a[2][2], a[3][2]);
-    b[3] = vec4_set(a[0][3], a[1][3], a[2][3], a[3][3]);
+//This can be a macro
+static inline void mat4_transpose(mat4 a) {
+    _MM_TRANSPOSE4_PS(a[0], a[1], a[2], a[3]);
 }
 
 static inline void mat4_cpy(mat4 a, mat4 b) {
@@ -148,7 +148,8 @@ static inline void mat4_cpy(mat4 a, mat4 b) {
 
 static inline vec4 vec4_mul_mat4(vec4 a, mat4 b) {
     mat4 t;
-    mat4_transpose(b, t);
+    mat4_cpy(b, t);
+    mat4_transpose(t);
 
     return vec4_xor(vec4_xor(vec4_dot(a, t[0], VEC4_DOT | VEC_STORE_0),
                              vec4_dot(a, t[1], VEC4_DOT | VEC_STORE_1)),
@@ -156,15 +157,17 @@ static inline vec4 vec4_mul_mat4(vec4 a, mat4 b) {
                              vec4_dot(a, t[3], VEC4_DOT | VEC_STORE_3)));
 }
 
+//Row major order matrix multiplication
 static inline void mat4_mul(mat4 a, mat4 b, mat4 c) {
     mat4 t;
-    mat4_transpose(b, t);
+    mat4_cpy(a, t);
+    mat4_transpose(t);
 
     for (int i = 0; i < 4; i++) {
-        c[i] = vec4_xor(vec4_xor(vec4_dot(a[i], t[0], VEC4_DOT | VEC_STORE_0),
-                                 vec4_dot(a[i], t[1], VEC4_DOT | VEC_STORE_1)),
-                        vec4_xor(vec4_dot(a[i], t[2], VEC4_DOT | VEC_STORE_2),
-                                 vec4_dot(a[i], t[3], VEC4_DOT | VEC_STORE_3)));
+        c[i] = vec4_xor(vec4_xor(vec4_dot(t[0], b[i], VEC4_DOT | VEC_STORE_0),
+                                 vec4_dot(t[1], b[i], VEC4_DOT | VEC_STORE_1)),
+                        vec4_xor(vec4_dot(t[2], b[i], VEC4_DOT | VEC_STORE_2),
+                                 vec4_dot(t[3], b[i], VEC4_DOT | VEC_STORE_3)));
     }
 }
 
@@ -174,36 +177,75 @@ typedef struct {
     vec4 up;
     vec4 right;
 
-    mat4 transform;
-
     float fov;
+    //Maybe add padding
 } camera_t;
 
-void update_camera_mat(camera_t* cam) {
-    /*TODO*/
-    //Ugly and unecessary
-    //and probably lacks optimization
-    vec4 world_up = vec4_set(0.0f, 1.0f, 0.0f, 1.0f);
-
-    cam->right = (vec4_crs(world_up, vec4_set(1.0f, 0.0f, 0.0f, 1.0f)));
-    cam->up    = vec4_crs(cam->right, cam->forward);
-    cam->right = vec4_crs(cam->forward, cam->up);
-
-    mat4 t;
-    t[0] = cam->right;
-    t[1] = cam->up;
-    t[2] = cam->forward;
-    t[3] = vec4_set(0.0f, 0.0f, 0.0f, 0.0f);
-
-    mat4_transpose(t, cam->transform);
+void update_camera(camera_t* cam, vec4 up) {
+    //Can this be optimized?
+    cam->right = vec4_nrm(vec4_crs(up          , cam->forward));
+    cam->up    =          vec4_crs(cam->forward, cam->right);
+    cam->right =          vec4_crs(cam->up     , cam->forward);
 }
 
+//Row major order matrix
+void get_view(camera_t* cam, mat4 m) {
+    m[0] = cam->right;
+    m[1] = cam->up;
+    m[2] = cam->forward;
+    //Maybe use a shuffle
+    m[3] = vec4_set(-cam->pos[0], -cam->pos[1], -cam->pos[2], 1.f);
+}
+
+void look_at(camera_t* cam) {
+}
+
+//Row major order matrix
+void perspective(mat4 m, float fovy, float aspect, float n, float f) {
+    float h = tan(fovy/2.f);
+
+    m[0] = vec4_set(1.f/(aspect*h), 0.f   , 0.f             ,  0.f);
+    m[1] = vec4_set(0.f           , 1.f/h , 0.f             ,  0.f);
+    m[2] = vec4_set(0.f           , 0.f   , -(f+n)/(f-n)    , -1.f);
+    m[3] = vec4_set(0.f           , 0.f   , -(2.f*f*n)/(f-n),  0.f);
+}
+
+typedef float vec3[3];
+
+static inline void vec3_to_vec4(vec4 v4, vec3 v3, float w) {
+    v4 = vec4_set(v3[0], v3[1], v4[2], w);
+}
+
+typedef float rect[2];
+
+typedef struct {
+    uint8_t* data;
+    size_t w;
+    size_t h;
+}buffer_t;
+
+void scanline_raster_triangle(vec4 v0, vec4 v1, vec4 v2, buffer_t* buff) {
+        
+}
+
+#define IMAGE_WIDTH  512
+#define IMAGE_HEIGHT 512
 int main() {
-    camera_t cam;
+    image_t* img = create_image(IMAGE_WIDTH, IMAGE_HEIGHT);
+    memset(img->data, 0, img->w*img->h*3);
 
-    cam.pos     = vec4_set(0.0f, 0.0f,  5.0f, 1.0f);
-    cam.forward = vec4_set(0.0f, 0.0f, -1.0f, 0.0f);
+    buffer_t buff;
+    buff.data = img->data;
+    buff.w    = img->w;
+    buff.h    = img->h;
 
-    mat4_print(cam.transform);
+    vec4 v0 = vec4_set( 0.f, 1.f, 0.f, 1.f);
+    vec4 v1 = vec4_set(-1.f, 0.f, 0.f, 1.f);
+    vec4 v2 = vec4_set( 1.f, 0.f, 0.f, 1.f);
+
+    scanline_raster_triangle(v0, v1, v2, &buff);
+
+    write_image(img, "test.ppm");
+    free_image(img);
     return 0;
 }

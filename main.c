67a838c7445176c4a8a14a72a8e97de111a59dcd
William Breno <williambreno@mat.ci.ufpb.br>
2018-09-27 09:26:19 -0300

    WIP

--- main.c
+++ main.c
@@ -7,245 +7,265 @@
 #include <xmmintrin.h>
 #include <smmintrin.h>
 
-//Fits in a 32bit register
-typedef struct {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} pixel_t;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-typedef struct {
-    uint8_t* data;
+#include "vmat.h"
+#include "ppm.h"
 
+struct buffer_container {
+    float *data;
     size_t w;
     size_t h;
-} image_t;
+};
 
-image_t* create_image(uint32_t w, uint32_t h) {
-    image_t* t = (image_t*)malloc(sizeof(image_t));
-    t->w = w;
-    t->h = h;
+#define FP_BITS 12
+#define FP_MASK ((1 << FP_BITS) - 1)
 
-        t->data = (uint8_t*)malloc(w*h*3);
+typedef int32_t fp32_t;
 
-    return t;
-}
+#define FP_FLOAT(x) ((x) * (float)(1 << FP_BITS))
+#define FP_TO_FLOAT(x) ((float)(x) / (float)(1 << FP_BITS))
+#define FP_MUL(a, b) ((((a)>>8)*((b)>>8))>>0)
 
-void free_image(image_t* t) {
-    free(t->data);
-    free(t);
-}
+#define MAX(a, b) ((a > b) ? a : b)
+#define MIN(a, b) ((a < b) ? a : b)
 
-void write_pixel(image_t* t, pixel_t p, uint32_t x, uint32_t y) {
-    uint32_t index = 3*(x + y * t->w);
+void raster_triangle(fp32_t *x, fp32_t *y, float *z, struct buffer_container *buff) 
+{
+    uint32_t max_x = MIN(MAX(x[0], MAX(x[1], x[2])), buff->w << FP_BITS) >> FP_BITS;
+    uint32_t max_y = MIN(MAX(y[0], MAX(y[1], y[2])), buff->w << FP_BITS) >> FP_BITS;
 
-    t->data[index    ] = p.r;
-    t->data[index + 1] = p.g;
-    t->data[index + 2] = p.b;
-}
+    uint32_t min_x = MAX(MIN(x[0], MIN(x[1], x[2])), 0) >> FP_BITS;
+    uint32_t min_y = MAX(MIN(y[0], MIN(y[1], y[2])), 0) >> FP_BITS;
 
-void clear_image(image_t* t, pixel_t p) {
-    for (int x = 0; x < t->w; x++) {
-        for (int y = 0; y < t->h; y++) {
-            write_pixel(t, p, x, y);
+    fp32_t A01 = y[0] - y[1];
+    fp32_t A12 = y[1] - y[2];
+    fp32_t A20 = y[2] - y[0];
+
+    fp32_t B01 = x[1] - x[0];
+    fp32_t B12 = x[2] - x[1];
+    fp32_t B20 = x[0] - x[2];
+
+    fp32_t w0_row = FP_MUL((min_x - x[1]),  (y[2] - y[1])) - FP_MUL((min_y - y[1]), (x[2] - x[1]));
+    fp32_t w1_row = FP_MUL((min_x - x[2]),  (y[0] - y[2])) - FP_MUL((min_y - y[2]), (x[0] - x[2]));
+    fp32_t w2_row = FP_MUL((min_x - x[0]),  (y[1] - y[0])) - FP_MUL((min_y - y[0]), (x[1] - x[0]));
+
+    /*
+    float depth;
+    float area = (x[2] - x[0]) * (y[1] - y[0]) - (y[2] - y[0]) * (x[1] - x[0]);
+    */
+
+    float xx[3] = {x[0] >> FP_BITS, x[1] >> FP_BITS, x[2] >> FP_BITS};
+    float yy[3] = {y[0] >> FP_BITS, y[1] >> FP_BITS, y[2] >> FP_BITS};
+
+    for (uint32_t py = min_y; py < max_y; py++) {
+        fp32_t w0 = w0_row;
+        fp32_t w1 = w1_row;
+        fp32_t w2 = w2_row;
+
+        for (uint32_t px = min_x; px < max_y; px++) {
+            fp32_t pxx = px << FP_BITS;
+            fp32_t pyy = py << FP_BITS;
+
+            fp32_t w0 = FP_MUL((pxx - x[1]),  (y[2] - y[1])) - FP_MUL((pyy - y[1]), (x[2] - x[1]));
+            fp32_t w1 = FP_MUL((pxx - x[2]),  (y[0] - y[2])) - FP_MUL((pyy - y[2]), (x[0] - x[2]));
+            fp32_t w2 = FP_MUL((pxx - x[0]),  (y[1] - y[0])) - FP_MUL((pyy - y[0]), (x[1] - x[0]));
+
+            if (w0 > 0 && w1 > 0 && w2 > 0) {
+                /*
+                w0 /= area;
+                w1 /= area;
+                w2 /= area;
+
+                depth = w0 * v0[2] + w1 * v1[2] + w2 * v2[2];
+                depth = fabs(depth);
+
+                //Demcompress Z
+                //depth = 1.f/depth;
+
+                uint32_t index = x + y * buff->w;
+
+
+                if (buff->data[index] > depth)
+                    buff->data[index] = depth;
+                    */
+
+                uint32_t index = px + py * buff->w;
+                buff->data[index] = 1.0f;
+            }
+
+            w0 += A12;
+            w1 += A20;
+            w2 += A01;
         }
+
+        w0_row += B12;
+        w1_row += B20;
+        w2_row += B01;
     }
 }
 
-int write_image(image_t* t, const char* file_path) {
-    FILE* file = fopen(file_path, "w");
+void raster_triangle_array(vec4 *vecs, size_t size, struct buffer_container *buff)
+{
+    mat4 ss;
+    screen_space(ss, buff->w, buff->h);
 
-    if (!file) {
-        fprintf(stderr, "ERROR: %s\n", strerror(errno));
-        return 0;
-    }
+    for (size_t i = 0; i < size; i++) {
+        fp32_t x[3];
+        fp32_t y[3];
+        float  z[3];
 
-    //PPM image
-    //Byte (P6) header
-    fprintf(file, "P6\n %lu %lu\n 255\n", t->w, t->h);
-    fwrite(t->data, 1, t->w * t->h * 3, file);
+        vec4 v0 = vec4_mul_mat4(vecs[i*3  ], ss);
+        vec4 v1 = vec4_mul_mat4(vecs[i*3+1], ss);
+        vec4 v2 = vec4_mul_mat4(vecs[i*3+2], ss);
 
-    return 1;
-}
+        x[0] = FP_FLOAT(v0[0] / v0[3]);
+        x[1] = FP_FLOAT(v1[0] / v1[3]);
+        x[2] = FP_FLOAT(v2[0] / v2[3]);
 
-typedef __m128 vec4;
+        y[0] = FP_FLOAT(v0[1] / v0[3]);
+        y[1] = FP_FLOAT(v1[1] / v1[3]);
+        y[2] = FP_FLOAT(v2[1] / v2[3]);
 
-#define vec4_set                _mm_setr_ps
-#define vec4_get                _mm_store_ps
-#define vec4_add                _mm_add_ps
-#define vec4_sub                _mm_sub_ps
-#define vec4_mul                _mm_mul_ps
-#define vec4_div                _mm_div_ps
-#define vec4_dot                _mm_dp_ps
-#define vec4_xor                _mm_xor_ps
-#define vec4_crs                _vec4_crs
-#define vec4_nrm                _vec4_nrm
-#define vec4_lng                _vec4_lng
-#define vec4_sum                _mm_hadds_ps
+        z[0] = v0[2] / v0[3];
+        z[1] = v1[2] / v1[3];
+        z[2] = v2[2] / v2[3];
 
-#define VEC2_DOT                0x30
-#define VEC3_DOT                0x70
-#define VEC4_DOT                0xF0
-
-#define VEC_STORE_0             0x01
-#define VEC_STORE_1             0x02
-#define VEC_STORE_2             0x04
-#define VEC_STORE_3             0x08
-#define VEC_STORE_ALL           0x0F
-
-static inline void vec4_print(vec4 a) {
-    printf("vec4 = [%.4f, %.4f, %.4f, %.4f]\n", a[0], a[1], a[2], a[3]);
-}
-
-static inline vec4 _vec4_crs(vec4 a, vec4 b) {
-    return vec4_sub(vec4_mul(_mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 0, 2, 1)),
-                             _mm_shuffle_ps(b, b, _MM_SHUFFLE(3, 1, 0, 2))),
-                    vec4_mul(_mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 1, 0, 2)),
-                             _mm_shuffle_ps(b, b, _MM_SHUFFLE(3, 0, 2, 1))));
-}
-
-/*TODO*/
-//Ugly, unecessary AND lacks optimization
-static inline vec4 _vec4_lng(vec4 a, const short m) {
-    vec4 dst;
-
-    vec4 tmp = vec4_mul(a, a);
-    float len = sqrt(tmp[0] + tmp[1] + tmp[2]);
-
-    for (int i = 0; i < 4; i++) {
-        if (m & VEC_STORE_0) dst[0] = len;
-        if (m & VEC_STORE_1) dst[1] = len;
-        if (m & VEC_STORE_2) dst[2] = len;
-        if (m & VEC_STORE_3) dst[3] = len;
-    }
-
-    return dst;
-}
-
-static inline vec4 _vec4_nrm(vec4 a) {
-    return vec4_div(a, vec4_lng(a, VEC_STORE_ALL));
-}
-
-typedef vec4 mat4[4];
-
-static inline void mat4_print(mat4 m) {
-    printf("mat4:\n");
-    for (int i = 0; i < 4; i++) {
-        printf("        ");
-        vec4_print(m[i]);
+        raster_triangle(x, y, z, buff);
     }
 }
 
-/*TODO*/
-//This can be a macro
-static inline void mat4_transpose(mat4 a) {
-    _MM_TRANSPOSE4_PS(a[0], a[1], a[2], a[3]);
+void render(struct buffer_container *buff)
+{
+    vec4 v0 = vec4_set( 0.f,  1.f, 1.f, 1.f);
+    vec4 v1 = vec4_set(-1.f, -1.f, 1.f, 1.f);
+    vec4 v2 = vec4_set( 1.f, -1.f, 1.f, 1.f);
+
+    vec4 vs[3] = {v0, v1, v2};
+
+    /*
+    mat4 v;
+    look_at(vec4_set(0.f, 1.f, 2.f, 1.f),
+            vec4_set(0.f, 0.f, 0.f, 0.f),
+            vec4_set(0.f, -1.f, 0.f, 0.f), v);
+
+    mat4 p;
+    perspective(p, 90.f, 1.33f, 0.1f, 100.f);
+
+    mat4 mvp;
+
+    mat4_mul(v, p, mvp);
+
+    vs[0] = vec4_mul_mat4(v0, mvp);
+    vs[1] = vec4_mul_mat4(v1, mvp);
+    vs[2] = vec4_mul_mat4(v2, mvp);
+    */
+
+    raster_triangle_array(vs, 1, buff);
 }
 
-static inline void mat4_cpy(mat4 a, mat4 b) {
-    for (int i = 0; i < 4; i++) b[i] = a[i];
-}
+int main()
+{
+    struct buffer_container buff;
+    buff.w = 512;
+    buff.h = 512;
+    buff.data = malloc(sizeof(float)*buff.w*buff.h);
 
-static inline vec4 vec4_mul_mat4(vec4 a, mat4 b) {
-    mat4 t;
-    mat4_cpy(b, t);
-    mat4_transpose(t);
-
-    return vec4_xor(vec4_xor(vec4_dot(a, t[0], VEC4_DOT | VEC_STORE_0),
-                             vec4_dot(a, t[1], VEC4_DOT | VEC_STORE_1)),
-                    vec4_xor(vec4_dot(a, t[2], VEC4_DOT | VEC_STORE_2),
-                             vec4_dot(a, t[3], VEC4_DOT | VEC_STORE_3)));
-}
-
-//Row major order matrix multiplication
-static inline void mat4_mul(mat4 a, mat4 b, mat4 c) {
-    mat4 t;
-    mat4_cpy(a, t);
-    mat4_transpose(t);
-
-    for (int i = 0; i < 4; i++) {
-        c[i] = vec4_xor(vec4_xor(vec4_dot(t[0], b[i], VEC4_DOT | VEC_STORE_0),
-                                 vec4_dot(t[1], b[i], VEC4_DOT | VEC_STORE_1)),
-                        vec4_xor(vec4_dot(t[2], b[i], VEC4_DOT | VEC_STORE_2),
-                                 vec4_dot(t[3], b[i], VEC4_DOT | VEC_STORE_3)));
+    for (size_t i = 0; i < buff.h*buff.w; i++) {
+        buff.data[i] = .1f;
     }
-}
 
-typedef struct {
-    vec4 pos;
-    vec4 forward;
-    vec4 up;
-    vec4 right;
+    render(&buff);
+    
+    /*
+    fp32_t a = FP_FLOAT(3.f);
+    fp32_t b = FP_FLOAT(6.8f);
 
-    float fov;
-    //Maybe add padding
-} camera_t;
+    fp32_t c = ((uint64_t)a * (uint64_t)b) >> FP_BITS;
+    */
 
-void update_camera(camera_t* cam, vec4 up) {
-    //Can this be optimized?
-    cam->right = vec4_nrm(vec4_crs(up          , cam->forward));
-    cam->up    =          vec4_crs(cam->forward, cam->right);
-    cam->right =          vec4_crs(cam->up     , cam->forward);
-}
+    /*
+    int32_t a = 3.0f *  (1 << FP_BITS);
+    int32_t b = -1.5f * (1 << FP_BITS);
 
-//Row major order matrix
-void get_view(camera_t* cam, mat4 m) {
-    m[0] = cam->right;
-    m[1] = cam->up;
-    m[2] = cam->forward;
-    //Maybe use a shuffle
-    m[3] = vec4_set(-cam->pos[0], -cam->pos[1], -cam->pos[2], 1.f);
-}
+    uint64_t c = ((uint64_t)a * (uint64_t)b) >> FP_BITS;
 
-void look_at(camera_t* cam) {
-}
+    int32_t mask_2 = 0xffffffff >> (32-FP_BITS);
 
-//Row major order matrix
-void perspective(mat4 m, float fovy, float aspect, float n, float f) {
-    float h = tan(fovy/2.f);
+    printf("%i %i\n", c >> FP_BITS, c & mask_2);
+    */
 
-    m[0] = vec4_set(1.f/(aspect*h), 0.f   , 0.f             ,  0.f);
-    m[1] = vec4_set(0.f           , 1.f/h , 0.f             ,  0.f);
-    m[2] = vec4_set(0.f           , 0.f   , -(f+n)/(f-n)    , -1.f);
-    m[3] = vec4_set(0.f           , 0.f   , -(2.f*f*n)/(f-n),  0.f);
-}
+    GLFWwindow *wnd = NULL;
 
-typedef float vec3[3];
+    glfwInit();
 
-static inline void vec3_to_vec4(vec4 v4, vec3 v3, float w) {
-    v4 = vec4_set(v3[0], v3[1], v4[2], w);
-}
+    glfwWindowHint(GLFW_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-typedef float rect[2];
+    wnd = glfwCreateWindow(buff.w, buff.h, "Z Buffer", NULL, NULL);
 
-typedef struct {
-    uint8_t* data;
-    size_t w;
-    size_t h;
-}buffer_t;
+    glfwMakeContextCurrent(wnd);
+    glfwSwapInterval(0);
 
-void scanline_raster_triangle(vec4 v0, vec4 v1, vec4 v2, buffer_t* buff) {
-        
-}
+    glClearColor(1.f, 1.f, 1.f, 1.f);
 
-#define IMAGE_WIDTH  512
-#define IMAGE_HEIGHT 512
-int main() {
-    image_t* img = create_image(IMAGE_WIDTH, IMAGE_HEIGHT);
-    memset(img->data, 0, img->w*img->h*3);
+    float delta_time;
+    float curr_time = 0.f;
+    float past_time = 0.f;
 
-    buffer_t buff;
-    buff.data = img->data;
-    buff.w    = img->w;
-    buff.h    = img->h;
 
-    vec4 v0 = vec4_set( 0.f, 1.f, 0.f, 1.f);
-    vec4 v1 = vec4_set(-1.f, 0.f, 0.f, 1.f);
-    vec4 v2 = vec4_set( 1.f, 0.f, 0.f, 1.f);
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
 
-    scanline_raster_triangle(v0, v1, v2, &buff);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    write_image(img, "test.ppm");
-    free_image(img);
-    return 0;
+    glEnable(GL_TEXTURE_2D);
+
+    while (!glfwWindowShouldClose(wnd)) {
+        glfwPollEvents();
+
+        curr_time = (float)glfwGetTime();
+        delta_time = past_time - curr_time;
+        past_time = curr_time;
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        render(&buff);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, buff.w, buff.h, 0, GL_RED, GL_FLOAT, buff.data);
+
+        glBegin(GL_TRIANGLES);
+            glTexCoord2f(0.f, 1.f);
+            glVertex3f(-1.f, -1.f, 0.f);
+            glTexCoord2f(1.f, 0.f);
+            glVertex3f( 1.f,  1.f, 0.f);
+            glTexCoord2f(0.f, 0.f);
+            glVertex3f(-1.f,  1.f, 0.f);
+
+            glTexCoord2f(0.f, 1.f);
+            glVertex3f(-1.f, -1.f, 0.f);
+            glTexCoord2f(1.f, 1.f);
+            glVertex3f( 1.f, -1.f, 0.f);
+            glTexCoord2f(1.f, 0.f);
+            glVertex3f( 1.f,  1.f, 0.f);
+        glEnd();
+
+        if (glfwGetKey(wnd, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(wnd, GLFW_TRUE);
+
+        glfwSwapBuffers(wnd);
+    }
+
+    glDeleteTextures(1, &tex);
+
+    glfwTerminate();
+
+    free(buff.data);
 }
